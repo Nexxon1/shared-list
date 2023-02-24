6915e3f55bee8f74202262f6f4b1dbe7793cd749
Mino Duelli <Minodu@freenet.de>
2023-02-24 14:15:08 +0100

    Cleanup generated classes and icons

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+      <h1 class="green">Shared List</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <HelloWorld msg="Todos" />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 @media (min-width: 1024px) {
+  h1 {
+    font-size: 2rem;
+    margin-top: 2rem;
+  }
+
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
